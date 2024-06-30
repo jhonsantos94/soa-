6118aaf17f4d211f42fcdec5b11ac202c3a58e53
Jhon Munarco Santos <jamunarco@espe.edu.ec>
2024-06-29 19:42:00 -0500

    first commit

---
+++ src/main/java/com/soap/client/SoapClient.java
@@ -0,0 +1,48 @@
+package com.soap.client;
+
+import com.soap.wsdl.*;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+public class SoapClient extends WebServiceGatewaySupport {
+
+    /**
+     * Metodo que se encarga de sumar dos numeros
+     *
+     * @param numberA
+     * @param numberB
+     * @return AddResponse
+     */
+    public AddResponse getAddResponse(int numberA, int numberB) {
+
+        Add addRequest = new Add();
+        addRequest.setIntA(numberA);
+        addRequest.setIntB(numberB);
+
+        SoapActionCallback soapActionCallback = new SoapActionCallback("http://tempuri.org/Add");
+
+        AddResponse addResponse = (AddResponse) getWebServiceTemplate().marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", addRequest, soapActionCallback);
+
+        return addResponse;
+    }
+
+
+    /**
+     * Metodo que encarga de restar dos numero
+     * @param numberA
+     * @param numberB
+     * @return SubtractResponse
+     */
+    public SubtractResponse getSubtractResponse(int numberA, int numberB) {
+
+        Subtract subtractRequest = new Subtract();
+        subtractRequest.setIntA(numberA);
+        subtractRequest.setIntB(numberB);
+
+        SoapActionCallback soapActionCallback = new SoapActionCallback("http://tempuri.org/Subtract");
+
+        SubtractResponse subtractResponse = (SubtractResponse) getWebServiceTemplate().marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", subtractRequest, soapActionCallback);
+
+        return subtractResponse;
+    }
+}
